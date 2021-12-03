afd5808dc72640228bcb4a6fac2bb8cc2dc0b647
gabriel-vas-dev <81338448+gabriel-vas-dev@users.noreply.github.com>
2021-12-03 17:13:01 -0400

    Atualizando atuação

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    // echo($_POST['selecionar'] . "<br>");
-    // echo($_POST['atuacao'] . "<br>");
-    // echo($_POST['nome'] . "<br>");
-    // echo($_POST['data_nascimento'] . "<br>");
-    // echo($_POST['cpf'] . "<br>");
-    // echo($_POST['rg'] . "<br>");
-    // echo($_POST['o_emissor'] . "<br>");
-    // echo($_POST['n_mae'] . "<br>");
-    // echo($_POST['n_pai'] . "<br>");
-    // echo($_POST['naturalidade'] . "<br>");
-    // echo($_POST['endereco'] . "<br>");
-    // echo($_POST['especialidade'] . "<br>");
-    // echo($_POST['n_conselho'] . "<br>");
-    // echo($_POST['telefone'] . "<br>");
-    // echo($_POST['e_mail'] . "<br>");
-
+  
     include_once('config.php');
 
     $selecionar = $_POST['selecionar'];
@@ -54,7 +39,6 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/index.css" rel="stylesheet" />
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <title>Formulário | HMVG</title>
 
 </head>
@@ -79,65 +63,37 @@ if (isset($_POST['submit'])) {
                 <input type="radio" id="atualizar" name="selecionar" value="atualizar" required>
                 <label for="atualizar"><b>ATUALIZAR: Atualizar cadastro</b></label><br><br>
 
-                <p><strong>Local de atuação:</strong></p>
+                <p><strong>Local de atuação:</strong></p><br>
                 <div>
-                    <input type="radio" id="uti_geral" name="atuacao" value="uti_geral" required>
-                    <label for="uti_geral">UTI Geral</label>
-                    <br>
-                    <input type="radio" id="Enfermarias" name="atuacao" value="Enfermarias" required>
-                    <label for="Enfermarias">Enfermarias</label>
-                    <br>
-                    <input type="radio" id="Administracao" name="atuacao" value="Administracao" required>
-                    <label for="uti_geral">UTI Geral</label>
-                    <br>
-                    <input type="radio" id="uti_geral" name="atuacao" value="uti_geral" required>
-                    <label for="Administracao">Administração</label>
-                    <br>
-                    <input type="radio" id="c_cirurgico" name="atuacao" value="c_cirurgico" required>
-                    <label for="c_cirurgico">Centro Cirurgico</label>
-                    <br>
-                    <input type="radio" id="fca" name="atuacao" value="fca" required>
-                    <label for="fca">Famacia/CAF ou almoxaifado</label>
-                    <br>
-                    <input type="radio" id="Recepções" name="atuacao" value="Recepções" required>
-                    <label for="recepcoes">Recepções</label>
-                    <br>
-                    <input type="radio" id="sesmt" name="atuacao" value="sesmt" required>
-                    <label for="sesmt">SESMT</label>
-                    <br>
-                    <input type="radio" id="cme" name="atuacao" value="cme" required>
-                    <label for="cme">CME</label>
-                    <br>
-                    <input type="radio" id="nutricao" name="atuacao" value="nutricao" required>
-                    <label for="nutricao">Nutrição</label>
-                    <br>
-                    <input type="radio" id="fisio" name="atuacao" value="fisio" required>
-                    <label for="fisio">Fisioterapia</label>
-                    <br>
-                    <input type="radio" id="uti_geral" name="atuacao" value="uti_geral" required>
-                    <label for="uti_mediall">UTI's MEDIALL</label>
-                    <br>
-                    <input type="radio" id="fono" name="atuacao" value="fono" required>
-                    <label for="fono">Fonoaudiologia</label>
-                    <br>
-                    <input type="radio" id="s_social" name="atuacao" value="s_social" required>
-                    <label for="s_social">Serviço social</label>
-                    <br>
-                    <input type="radio" id="s_social" name="atuacao" value="outros" required>
-                    <label for="s_social">Outros</label>
-                    <br><br><br>
+                <div class="inputBox">
+                    <label for="atuacao" class="labelInput teste"><b>Local de Trabalho</b></label>
+                    <select name="atuacao" id="atuacao" class="estados">
+                        <option value="AC">UTI Geral</option>
+                        <option value="AL">Enfermarias</option>
+                        <option value="AP">Administração</option>
+                        <option value="AM">Centro Cirurgico</option>
+                        <option value="BA">Famacia/CAF</option>
+                        <option value="CE">Almoxaifado</option>
+                        <option value="DF">Recepções</option>
+                        <option value="ES">SESMT</option>
+                        <option value="GO">CME</option>
+                        <option value="MA">Nutrição</option>
+                        <option value="MT">Fisioterapia</option>
+                        <option value="MS">Fonoaudiologia</option>
+                        <option value="MG">Serviço social</option>
+                        <option value="MG">Tecnologia da Informação</option>
+                        <option value="PA">Outros</option>
+                        </option><br><br><br><br><br>
+                    </select>
+
+
+                </div><br><br>
                 </div>
 
-                <!-- <div class="input-group flex-nowrap inputBox">
-                    <input type="text" class="form-control inputUser">
-                    <label for="nome" class="labelInput">Nome completo</label>
-                </div>
-                <br>
-                <br> -->
 
                 <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label class="labelInput" for="nome"><b>Nome completo</b></label>
+                    <input type="text" name="nome" id="nome" class="inputUser" required><BR>
+                    <label for="nome" class="labelInput"><b>Nome completo</b></label>
                 </div><br><br>
 
                 <div class="input-group">
@@ -171,7 +127,7 @@ if (isset($_POST['submit'])) {
                 </div> <br><br>
 
                 <div class="inputBox">
-                    <label for="naturalidade" class="labelInput teste"><b>Naturalidade (cidade onde nasceu)</b></label>
+                    <label for="naturalidade" class="labelInput teste"><b>Naturalidade (estado onde nasceu)</b></label>
                     <select name="naturalidade" id="naturalidade" class="estados">
                         <option value="AC">Acre-AC</option>
                         <option value="AL">Alagoas-AL</option>
@@ -240,6 +196,4 @@ if (isset($_POST['submit'])) {
         </script> -->
     </div>
 </body>
-<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>-->
-
 </html>
